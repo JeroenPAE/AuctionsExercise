180ac7bd90ddc32ca71e4d496085ce5d7f6cae13
Jeroen.paesen <jeroen.paesen@mobitrace.eu>
2020-04-23 11:07:19 +0200

    First test make, lots to do

--- src/test/java/be/pxl/auctions/model/UserGetAgeTest.java
+++ src/test/java/be/pxl/auctions/model/UserGetAgeTest.java
@@ -2,22 +2,33 @@ package be.pxl.auctions.model;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class UserGetAgeTest {
 
+	private final int YEARS = 15;
 
 	@Test
 	public void returnsCorrectAgeWhenHavingBirthdayToday() {
-		// TODO implement test
+		User user = new User();
+		user.setDateOfBirth(LocalDate.now().minusYears(YEARS));
+		assertEquals(user.getAge(), YEARS);
 	}
 
 	@Test
 	public void returnsCorrectAgeWhenHavingBirthdayTomorrow() {
-		// TODO implement test
+		User user = new User();
+		user.setDateOfBirth(LocalDate.now().minusYears(YEARS).plusDays(1));
+		assertEquals(user.getAge(), YEARS -1 );
 	}
 
 	@Test
 	public void returnsCorrectAgeWhenBirthdayWasYesterday() {
-		// TODO implement test
+		User user = new User();
+		user.setDateOfBirth(LocalDate.now().minusYears(YEARS).plusDays(1));
+		assertEquals(user.getAge(), YEARS -1 );
 	}
 
 }
